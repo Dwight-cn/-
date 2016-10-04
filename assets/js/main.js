5cb719e0d88d40b0805e3aa95072b3715c11c993
Dwight <15629135565@163.com>
2016-10-05 00:32:22 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,8 @@ require.config({
    }
 });
 var imgid = 0,
-    textid = 0;
+    textid = 0,
+    moving;
 require(['jquery','window','jqueryUI','draw'], function($,w,$UI,d){
     $('#img-list').delegate('.img-container', 'click', function(e){
         $('.img-box').removeClass('active-box');
@@ -28,6 +29,7 @@ require(['jquery','window','jqueryUI','draw'], function($,w,$UI,d){
         var fontSize = document.getElementById('font-size').value ||36;
         var fontColor = document.getElementById('font-color').value;
         var fontFamily = document.getElementById('font-family').value;
+        //alert(fontContent+fontSize+fontColor+fontFamily);
         var text = new w.Window();
         text.init('.panel','<span class="text" style = "font:'+fontSize+'px'+' '+fontFamily+'; color:'+fontColor+'"; id="text-'+textid+'">'+fontContent+'</span>')
         imgid++;
@@ -41,6 +43,7 @@ require(['jquery','window','jqueryUI','draw'], function($,w,$UI,d){
         }
     }).resizable({containment:'.center'});
 
+
     $('#clear-btn').click(function(){
        $('.img-box').remove();
     });
@@ -52,5 +55,22 @@ require(['jquery','window','jqueryUI','draw'], function($,w,$UI,d){
 
     $('#msk').click(function(){
         $(this).toggleClass('show');
+    });
+
+    $('input[type = file]').change(function(e){
+        var reader = new FileReader();
+        var imgSrc = '',
+            isImg = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
+        if(!isImg.test(e.target.files[0].type)){
+            alert("只能添加图片呦喂！\n\nThisType:"+ e.target.files[0].type);
+            return;
+        }
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload = function(e) {
+            imgSrc = e.target.result;
+            var imgDiv = $('<div class="img-container"><img src="" alt=""/></div>');
+            $('#img-list').prepend(imgDiv);
+            imgDiv.children()[0].src = imgSrc;
+        };
     });
 });
